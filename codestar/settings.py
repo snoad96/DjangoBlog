1deaa09a9530467b8805b01432d21a9348114756
DSnoad <danialsnoad@hotmail.com>
2023-01-24 11:20:05 +0000

    Minor bug fix

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ["mb2test.herokuapp.com", "localhost"]
+ALLOWED_HOSTS = ["my-first-blog.herokuapp.com/", "localhost"]
 
 # Application definition
 
